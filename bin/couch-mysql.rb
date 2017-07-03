95461e0c651237346e6b809ea8a42fe83642a51f
mangochiman <mangochiman@gmail.com>
2017-07-03 15:03:19 +0200

    Added couch_mysql.yml.example

--- bin/couch-mysql.rb
+++ bin/couch-mysql.rb
@@ -1,8 +1,8 @@
 require 'couch_tap'
 
-changes "http://user:pass@host:port/database" do
+changes "http://mangochiman:mangochiman@localhost:5984/ebrs_hq" do
   # Which database should we connect to?
-  database "adapter://user:pass@host:port/test_database"
+  database "mysql2://root:ernest@localhost:3306/bart2_new"
 	  document 'type' => 'User' do
 	    table :user
 	  end
