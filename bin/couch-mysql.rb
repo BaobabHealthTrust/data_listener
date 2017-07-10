af97e3e8384723e71d6da4e80ce38563dc50574e
mangochiman <mangochiman@gmail.com>
2017-07-10 15:20:03 +0200

    Updating MySQL database manually

--- bin/couch-mysql.rb
+++ bin/couch-mysql.rb
@@ -1,5 +1,8 @@
+
 require 'couch_tap'
 require "yaml"
+require 'mysql2'
+
 DIR = File.dirname(__FILE__)
 
 #DIR = File.dirname(__FILE__)
@@ -27,29 +30,112 @@ mysql_adapter = mysql_db_settings["adapter"]
 db_map_path = DIR.to_s + "/config/db_mapping.yml"
 db_maps = YAML.load_file(db_map_path)
 
+client = Mysql2::Client.new(:host => mysql_host,
+  :username => mysql_username,
+  :password => mysql_password,
+  :database => mysql_db
+)
+
 changes "http://#{couch_username}:#{couch_password}@#{couch_host}:#{couch_port}/#{couch_db}" do
   # Which database should we connect to?
   database "#{mysql_adapter}://#{mysql_username}:#{mysql_password}@#{mysql_host}:#{mysql_port}/#{mysql_db}"
+  #StatusCouchdb Document Type
+  document 'type' => "StatusCouchdb" do |doc|
+    #doc_id = doc.document["_id"]
+    status_id = doc.document["status_id"]
+    name = doc.document["name"]
+    description = doc.document["description"]
+    updated_at = doc.document["updated_at"].to_date rescue doc.document["updated_at"]
+    created_at = doc.document["created_at"].to_date rescue doc.document["created_at"]
 
-  	 map_keys = db_maps.keys
+    mysql_status_record = client.query("SELECT * FROM statuses WHERE status_id = '#{status_id}' LIMIT 1").each(:as => :hash)
+    if mysql_status_record.blank?
+      insert_status_statement = "INSERT INTO statuses (status_id, name, description, created_at, updated_at) VALUES ('#{status_id}', '#{name}', '#{description}', '#{created_at}', '#{updated_at}')"
+      client.query(insert_status_statement)
+      
+    else
+      update_status_statement = "UPDATE statuses SET name = '#{name}', description = '#{description}', updated_at = '#{updated_at}' WHERE status_id = '#{status_id}'"
+      client.query(update_status_statement)
+    end
+  end
 
-  	 map_keys.each do |map|
+  #BirthRegistrationTypeCouchdb Document Type
+  document 'type' => "BirthRegistrationTypeCouchdb" do |doc|
+    #doc_id = doc.document["_id"]
+    birth_registration_type_id = doc.document["birth_registration_type_id"]
+    name = doc.document["name"]
+    updated_at = doc.document["updated_at"].to_date rescue doc.document["updated_at"]
+    created_at = doc.document["created_at"].to_date rescue doc.document["created_at"]
 
-  	 	  map_parts = map.split("|")
-  	 	  document_type = map_parts [0]
-  	 	  db_table = map_parts[1]
+    mysql_birth_registration_type_record = client.query("SELECT * FROM birth_registration_type WHERE birth_registration_type_id = '#{birth_registration_type_id}' LIMIT 1").each(:as => :hash)
 
-  	 	  couch_db_fields = db_maps[map].keys
+    if mysql_birth_registration_type_record.blank?
+      insert_birth_registration_type_statement = "INSERT INTO birth_registration_type (birth_registration_type_id, name, created_at, updated_at) VALUES ('#{birth_registration_type_id}', '#{name}', '#{created_at}', '#{updated_at}')"
+      client.query(insert_birth_registration_type_statement)
+    else
+      update_birth_registration_type_statement = "UPDATE birth_registration_type SET name = '#{name}', updated_at = #{updated_at} WHERE birth_registration_type_id = '#{birth_registration_type_id}'"
+      client.query(update_birth_registration_type_statement)
+    end
+  end
 
-		  document 'type' => document_type do
+  #PersonTypeOfBirthsCouchdb Document Type
+  document 'type' => "PersonTypeOfBirthsCouchdb" do |doc|
+    #doc_id = doc.document["_id"]
+    person_type_of_birth_id = doc.document["person_type_of_birth_id"]
+    name = doc.document["name"]
+    description = doc.document["description"]
+    updated_at = doc.document["updated_at"].to_date rescue doc.document["updated_at"]
+    created_at = doc.document["created_at"].to_date rescue doc.document["created_at"]
 
-			    eval("table :#{db_table}") do
-			    	
-			    	couch_db_fields.each do |field|
-			    		eval("column :#{field},:#{db_maps[map][field]}")
-			    	end
-			    end
-		  end
-	 end
+    mysql_person_type_of_birth_record = client.query("SELECT * FROM person_type_of_births WHERE person_type_of_birth_id = '#{person_type_of_birth_id}' LIMIT 1").each(:as => :hash)
+    if mysql_person_type_of_birth_record.blank?
+      insert_person_type_of_birth_statement = "INSERT INTO person_type_of_births (person_type_of_birth_id, name, description, created_at, updated_at) VALUES ('#{person_type_of_birth_id}', '#{name}', '#{description}', '#{created_at}', '#{updated_at}')"
+      client.query(insert_person_type_of_birth_statement)
+
+    else
+      update_person_type_of_birth_statement = "UPDATE person_type_of_births SET name = '#{name}', description = '#{description}', updated_at = '#{updated_at}' WHERE person_type_of_birth_id = '#{person_type_of_birth_id}'"
+      client.query(update_person_type_of_birth_statement)
+    end
+  end
+
+  #PersonTypeCouchdb Document Type
+  document 'type' => "PersonTypeCouchdb" do |doc|
+    #doc_id = doc.document["_id"]
+    person_type_id = doc.document["person_type_id"]
+    name = doc.document["name"]
+    description = doc.document["description"]
+    updated_at = doc.document["updated_at"].to_date rescue doc.document["updated_at"]
+    created_at = doc.document["created_at"].to_date rescue doc.document["created_at"]
+
+    mysql_person_type_record = client.query("SELECT * FROM person_type WHERE person_type_id = '#{person_type_id}' LIMIT 1").each(:as => :hash)
+    if mysql_person_type_record.blank?
+      insert_person_type_statement = "INSERT INTO person_type (person_type_id, name, description, created_at, updated_at) VALUES ('#{person_type_id}', '#{name}', '#{description}', '#{created_at}', '#{updated_at}')"
+      client.query(insert_person_type_statement)
+
+    else
+      update_person_type_statement = "UPDATE person_type SET name = '#{name}', description = '#{description}', updated_at = '#{updated_at}' WHERE person_type_id = '#{person_type_id}'"
+      client.query(update_person_type_statement)
+    end
+  end
+
+  #PersonRelationshipTypesCouchdb Document Type
+  document 'type' => "PersonRelationshipTypesCouchdb" do |doc|
+    #doc_id = doc.document["_id"]
+    person_relationship_type_id = doc.document["person_relationship_type_id"]
+    name = doc.document["name"]
+    description = doc.document["description"]
+    updated_at = doc.document["updated_at"].to_date rescue doc.document["updated_at"]
+    created_at = doc.document["created_at"].to_date rescue doc.document["created_at"]
+
+    mysql_person_relationship_type_record = client.query("SELECT * FROM person_relationship_types WHERE person_relationship_type_id = '#{person_relationship_type_id}' LIMIT 1").each(:as => :hash)
+    if mysql_person_relationship_type_record.blank?
+      insert_person_relationship_type_statement = "INSERT INTO person_relationship_types (person_relationship_type_id, name, description, created_at, updated_at) VALUES ('#{person_relationship_type_id}', '#{name}', '#{description}', '#{created_at}', '#{updated_at}')"
+      client.query(insert_person_relationship_type_statement)
+
+    else
+      update_person_relationship_type_statement = "UPDATE person_relationship_types SET name = '#{name}', description = '#{description}', updated_at = '#{updated_at}' WHERE person_relationship_type_id = '#{person_relationship_type_id}'"
+      client.query(update_person_relationship_type_statement)
+    end
+  end
 
 end
